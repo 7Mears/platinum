df30342e668c4aa815a6c00bcd8d8f436e19e94b
7mears <7mears@gmail.com>
2015-02-11 19:27:29 -0600

    navigation style, button styles, home section markup cleanup

--- page-home.php
+++ page-home.php
@@ -10,25 +10,22 @@ get_header(); ?>
 
 <main id="main" class="site-main" role="main">
 
-  <section id="home-hero" class="home-hero">
+  <section class="home-hero">
     <?php dynamic_sidebar( 'home-hero' ); ?>
 
     <div class="home-hours">
       We are open weekdays from 8:00 am to 5:00 pm, closed on weekends
     </div>
-  </section>
-  <!-- /home-hero -->
+  </section><!-- /home-hero -->
 
-  <div class="wrapper">
-    <?php if ( is_active_sidebar( 'home-content' ) ) : ?>
-    <section id="home-content" class="home-content">
-
+  <section class="home-services">
+    <div class="wrapper">
       <h1>Repair, Paint, Services</h1>
-      <p>SGI accredited auto body shop serving Saskatoon and surrounding areas. Our collision repair technicians are paid by the day, not by the job, meaning they'll take the time to do your repairs correctly.</p>
+      <p>SGI accredited auto body shop serving Saskatoon and surrounding areas.</p>
 
       <div class="card">
         <img src="/platinum/wp-content/themes/platinum/src/img/training.svg">
-        <h2>Collision repair</h2>
+        <h2>Collision Repair</h2>
         <p>Proactively productize pandemic partnerships before future-proof platforms. Compellingly monetize.</p>
       </div>
 
@@ -40,20 +37,43 @@ get_header(); ?>
 
       <div class="card">
         <img src="/platinum/wp-content/themes/platinum/src/img/responsive.svg">
-        <h2>Mechanical</h2>
+        <h2>Paintless Dent Repair</h2>
         <p>Proactively productize pandemic partnerships before future-proof platforms. Compellingly monetize.</p>
       </div>
 
       <div class="card">
         <img src="/platinum/wp-content/themes/platinum/src/img/training.svg">
-        <h2>Frame Repair</h2>
+        <h2>Glass Installation</h2>
         <p>Proactively productize pandemic partnerships before future-proof platforms. Compellingly monetize.</p>
       </div>
 
-      <?php dynamic_sidebar( 'home-content' ); ?>
-    </section>
-    <!-- /home-content -->
-    <?php endif; ?>
-  </div>
+      <a class="button" href="#0">See a full list of our services</a>
+    </div><!-- /wrapper -->
+  </section><!-- /home-services -->
+
+  <section class="home-about">
+    <div class="wrapper">
+      <div class="home-about--content">
+        <h1>Proud to be a quality straight time shop</h1>
+        <p>We are proud to be a quality straight time shop versus a flat rate shop where technicians take short cuts on jobs to log more hours. Straight time shops provide better quality and save customers unnecessary trips back for warranty repairs.</p>
+        <a class="button" href="#0">Find out more about us</a>
+      </div>
+    </div><!-- /wrapper -->
+  </section><!-- /home-about -->
+
+  <section class="home-contact">
+    <div class="wrapper">
+      <h1>Contact us</h1>
+      <ul>
+        <li>Store Hours</li>
+        <li>Monday: 7am - 5pm</li>
+        <li>Tuesday: 7am - 5pm</li>
+        <li>Wednesday: 7am - 5pm</li>
+        <li>Thursday: 7am - 5pm</li>
+        <li>Friday: 7am - 5pm</li>
+      </ul>
+    </div><!-- /wrapper -->
+  </section><!-- /home-contact -->
+
 </main><!-- /main -->
 <?php get_footer(); ?>
