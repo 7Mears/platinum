a89c3ade6e229cddfec6a81d263142ac8e7b46bb
7mears <7mears@gmail.com>
2015-02-15 10:53:49 -0600

    started responsive homepage

--- header.php
+++ header.php
@@ -33,7 +33,6 @@
 		</div><!-- .contact-info -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'platinum' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 
